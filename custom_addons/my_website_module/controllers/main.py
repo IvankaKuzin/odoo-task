7c14fe8aea42d41028eb584be10cdacca5bde4ee
IvankaKuzin <kuziniwanka@gmail.com>
2025-05-07 17:17:25 +0300

    changed model name in controller and view

--- custom_addons/my_website_module/controllers/main.py
+++ custom_addons/my_website_module/controllers/main.py
@@ -8,8 +8,8 @@ class PersonsController(http.Controller):
 
     @http.route("/persons", type="http", auth="public", website=True)
     def persons_page(self, **kw):
-        persons = request.env["persons.person"].sudo().search([], limit=5, order="create_date desc")
+        persons = request.env["my_website.person"].sudo().search([], limit=5, order="create_date desc")
 
-        return request.render("persons_module.persons_page_template", {
+        return request.render("my_website_module.persons_page_template", {
             "persons": persons
         })
