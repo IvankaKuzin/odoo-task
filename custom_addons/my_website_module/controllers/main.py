817a92516b5ffd8a6aff26051bf7319fd6b8793a
IvankaKuzin <kuziniwanka@gmail.com>
2025-05-07 20:31:41 +0300

    created a method to get data and database record

--- custom_addons/my_website_module/controllers/main.py
+++ custom_addons/my_website_module/controllers/main.py
@@ -20,3 +20,29 @@ class PersonsController(http.Controller):
         return request.render("my_website_module.person_add_form_template", {
             "companies": companies
         })
+
+    @http.route("/persons/create", type="http", auth="public", website=True, methods=["POST"])
+    def person_create(self, **post):
+        if post.get("first_name") and post.get("last_name") and post.get("company_id"):
+            company_id = int(post.get("company_id"))
+
+            person_values = {
+                "first_name": post.get("first_name"),
+                "last_name": post.get("last_name"),
+                "company_id": company_id,
+                "sex": post.get("sex") or False,
+            }
+
+            if post.get("birthday"):
+                person_values["birthday"] = post.get("birthday")
+
+            request.env["my_website.person"].sudo().create(person_values)
+
+            return request.redirect("/persons")
+
+        companies = request.env["res.company"].sudo().search([])
+        return request.render("my_website_module.person_add_form_template", {
+            "companies": companies,
+            "error": "Please fill all required fields.",
+            "post": post,
+        })
